760e622b1bb3e71246e327104f532a097dc0169f
Researcher-H <researcher.hhh@gmail.com>
2020-06-14 00:47:37 +0100

    add image files

--- js/basics.js
+++ js/basics.js
@@ -3,3 +3,21 @@
 var word = document.querySelector("#js1");
 // console.log(word.innerHTML); // this is for sanity check
 word.innerHTML = "Hello world, from js file";
+
+// js-02 //
+
+// https://www.quora.com/How-do-you-insert-an-image-in-Javascript
+
+var src = document.querySelector("#js2"); // find target element
+// console.log(src); // this is for sanity check
+var img = document.createElement("img"); // create element first
+
+img.src = "images/dog1.jpg"; // assign an image to the element
+img.width = "300"; // assign an image to the element
+img.alt = "dog1";
+img.title = "dog1";
+
+src.innerHTML = ""; // remove strings
+
+src.appendChild(img); // assign the image element to the target element
+
